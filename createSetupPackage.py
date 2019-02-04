107fa5456b655c067da0902a213961f4b5bdf933
rubenmeeuw <r.d.meeuwissen@student.tudelft.nl>
2019-02-04 15:40:49 +0100

    The first working version of the website grabber

---
+++ createSetupPackage.py
@@ -0,0 +1,27 @@
+from shutil import copyfile
+from subprocess import call
+from resources.websiteCrawler import Grabber
+from resources.Converter import ( LinkConverter, UrlConverter )
+import os
+import json
+
+def createOutputFolder():
+	""" Create an output folder if does not exit yet """
+	newpath = r'output'
+	if not os.path.exists(newpath):
+		os.makedirs(newpath)
+
+def main():
+	""" The main function of the program """
+	# Load the config file
+	configfile = 'config.json'
+	with open(configfile, 'r') as f:
+		config = json.load(f)
+
+	createOutputFolder()
+
+	grabber = Grabber(config)
+	grabber.execute()
+
+if __name__ == '__main__':
+	main()
---
+++ resources/websiteCrawler.py
@@ -0,0 +1,61 @@
+from subprocess import call
+import shutil
+import os
+from Converter import ( UrlConverter, LinkConverter )
+# import config
+import json
+
+class Grabber:
+
+	def __init__(self, config):
+		"""Initialise a web grabber."""
+
+		self.config = config
+		# Load the necessary variables from the config file
+		self.website_list = self.config['WEB']['CRAWL_LIST']
+		self.output = self.config['WEB']['WEB_OUTPUT']
+		self.output_grabbed_websites = self.output + '/grabbedWebsites'
+		self.install_web = self.config['WEB']['WEB_INSTALL_FILE']
+		self.ip_prefix = self.config["IP_PREFIX"]
+
+		self.urlConverter = UrlConverter(self.config)
+		self.linkConverter = LinkConverter(self.config)
+
+		# TODO set this option as a command
+		self.convertLinks = False
+
+	def getWebsite(self, url):
+		"""Use wget command to retrieve recursively all files of website"""
+
+		print "Crawling {} from the web...".format(url)
+		call(['wget', url, '--recursive', '--convert-links',
+		'--page-requisites', '--continue', '--tries=5',
+		'--directory-prefix=' + self.output_grabbed_websites + "/" + self.urlConverter.convertUrl(url), '--retry-connrefused',
+		'--quiet', '-nH', '--no-parent'])
+
+	def getWebsiteFromFile(self):
+		"""Read all lines from the website list and execute the getWebsite method"""
+
+		with open(self.website_list, "r") as fp:
+	    		for line in fp:
+	        		self.getWebsite(line.rstrip('\n'))
+
+	def clearGrabbedFolder(self):
+		if os.path.exists(self.output_grabbed_websites):
+			shutil.rmtree(self.output_grabbed_websites)
+
+	def execute(self):
+		"""Start the grabbing process"""
+		# Remove all old files
+		self.clearGrabbedFolder()
+
+		# Create new url names for the websites
+		self.urlConverter.convertUrlsInFile()
+
+		# Get all website sources from WEBSITES_FILE
+		self.getWebsiteFromFile()
+
+		# Optionally convert the links in the grabbed website to point to each otherself.
+		if (self.convertLinks):
+			for f in os.listdir(self.output_grabbed_websites):
+				self.linkConvert.convertFolder(os.path.join(self.output_grabbed_websites, f))
